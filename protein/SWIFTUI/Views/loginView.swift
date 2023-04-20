9a858f2fb8b1d75182637a5edb49a88bb265aa17
cobena <cobenaa11@gmail.com>
2023-04-20 18:57:58 -0400

    Initial commit for AWS Amplify

---
+++ protein/SWIFTUI/Views/loginView.swift
@@ -0,0 +1,20 @@
+//
+//  loginView.swift
+//  protein
+//
+//  Created by Ariel Cobena on 4/20/23.
+//
+
+import SwiftUI
+
+struct loginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct loginView_Previews: PreviewProvider {
+    static var previews: some View {
+        loginView()
+    }
+}
--- protein/proteinApp.swift
+++ protein/proteinApp.swift
@@ -6,9 +6,33 @@
 //
 
 import SwiftUI
+import Amplify
+import AWSCognitoAuthPlugin
 
 @main
 struct proteinApp: App {
+    
+    func fetchCurrentAuthSession() async {
+        do {
+            let session = try await Amplify.Auth.fetchAuthSession()
+            print("Is user signed in - \(session.isSignedIn)")
+        } catch let error as AuthError {
+            print("Fetch session failed with error \(error)")
+        } catch {
+            print("Unexpected error: \(error)")
+        }
+    }
+    
+    init() {
+            do {
+                try Amplify.add(plugin: AWSCognitoAuthPlugin())
+                try Amplify.configure()
+                print("Amplify configured with auth plugin")
+            } catch {
+                print("Failed to initialize Amplify with \(error)")
+            }
+        }
+    
     var body: some Scene {
         WindowGroup {
             homeView()
