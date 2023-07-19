807370e94f1a90a7586377a15a37a0394bbf02db
Tyler Hernandez <tylerh6201@gmail.com>
2023-07-19 19:25:57 -0400

    Adds trends view to calendar view

--- protein/SWIFTUI/Views/calendarView.swift
+++ protein/SWIFTUI/Views/calendarView.swift
@@ -37,8 +37,15 @@ struct datePicker: View {
     
     @State public var dates: Set<DateComponents> = []
     
+    @State private var showPopup = false
+    
     var body: some View {
         VStack{
+            
+            Button("Trends") {
+                showPopup = true
+            }
+            
             MultiDatePicker("Dates Available", selection: $dates, in: bounds)
                 .fixedSize()
             
@@ -56,6 +63,13 @@ struct datePicker: View {
             Text("total sum across \(dates.count) entries: \(sumEntries())")
             
             Text("Average:  \(retrieveAverageFromSelected())")
+        }
+        .popover(isPresented: $showPopup) {
+                NavigationView {
+                    showTrendView()
+                    .navigationBarTitle("Trends")
+                }
+                
         }
         
     }
@@ -122,8 +136,85 @@ struct calendarView: View {
     } // ends view
 }
 
+
+struct showTrendView: View {
+    
+    @State private var date = Calendar.current.startOfDay(for: Date.now)
+    
+    var body: some View {
+        
+        VStack(spacing: 20) {
+                    NavigationLink(destination: detailView(text: generateTrend(days: 7))) {
+                        Text(generateTrend(days: 7))
+                    }
+            NavigationLink(destination: detailView(text: generateTrend(days: 14))) {
+                Text(generateTrend(days: 14))
+            }
+            NavigationLink(destination: detailView(text: generateTrend(days: 30))) {
+                Text(generateTrend(days: 30))
+            }
+            NavigationLink(destination: detailView(text: generateTrend(days: 60))) {
+                Text(generateTrend(days: 60))
+            }
+            NavigationLink(destination: detailView(text: generateTrend(days: 90))) {
+                Text(generateTrend(days: 90))
+            }
+                }
+    }
+    
+    // Past 7 days.
+    func generateTrend(days: Int) -> String {
+        
+        var totalGrams = 0
+        var currentDate = date
+        
+        // Start off with day before and work back x days.
+        for daysAgo in 1 ... days {
+            currentDate = Calendar.current.date(byAdding: .day, value: -daysAgo, to: date)!
+            
+            
+            if let value = Int(loadProteinFromStorage(date: currentDate)){
+                totalGrams += value
+                //print("\(currentDate) - \(value)")
+            } else {
+                return "Past \(days) days: Unavailable Trend"
+            }
+            
+        }
+        
+        return "Past \(days) days: Average \(totalGrams / days) grams per day"
+    }
+    
+    func loadProteinFromStorage(date: Date) -> String {
+
+        let defaults = UserDefaults.standard
+
+        let key = (date.formatted(date: .long, time: .omitted))
+
+        if let storedIntake = defaults.string(forKey: key) {
+            return storedIntake
+        } else {
+            return "0"
+        }
+    }
+    
+}
+
+struct detailView: View {
+    
+    var text: String
+
+    var body: some View {
+        Text(text)
+            .navigationBarTitle("Detail")
+    }
+    
+}
+
 struct calendarView_Previews: PreviewProvider {
     static var previews: some View {
         calendarView().preferredColorScheme(.dark)
     }
 }
+
+
