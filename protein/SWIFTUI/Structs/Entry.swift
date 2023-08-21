89f7dc84efd0ff4703d9fec61a15a0fa18207fc2
Tyler Hernandez <tylerh6201@gmail.com>
2023-08-20 20:15:04 -0400

    Adds modification of entries via tap and hold gesture, allows deletion of entries via swiping from right to left on an entry.

--- protein/SWIFTUI/Structs/Entry.swift
+++ protein/SWIFTUI/Structs/Entry.swift
@@ -7,23 +7,23 @@
 
 import Foundation
 
-struct Entry : Hashable {
+struct Entry : Hashable, Equatable, Identifiable {
     let grams : Int
     let id = UUID()
     // TODO: Add timestamp here, will also need to change storage and retrieval mechanism.
 }
 
 extension [Entry] {
-    
     func findEntry(Entry: Int) -> Array.Index? {
-        
-        let List = self
-        
-        for item in List.indices {
-            if List[item].grams == Entry {
-                return item
-            }
-        }
-        return nil
-    }
+         
+         let List = self
+         
+         for item in List.indices {
+             if List[item].grams == Entry {
+                 return item
+             }
+         }
+         return nil
+     }
+
 }
